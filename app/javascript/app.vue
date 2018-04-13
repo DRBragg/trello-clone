f95260c97074bf7f77dcb4b060f6d4139e153709
DRBragg <DRBragg@gmail.com>
2018-04-13 09:59:59 -0400

    started connectin Vue to Rails controllers for persistance. Added vue-draggable and set up lists to be drag and drop

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,24 +1,68 @@
 <template>
-  <div id="app" class="row">
-    <div v-for="list in original_lists" class="col-3">
+  <draggable v-model="lists" :options="{group: 'lists'}" @end="listMoved" class="row dragArea">
+    <div v-for="(list, index) in lists" class="col-3">
       <h6>{{ list.name }}</h6>
       <hr />
-      <div v-for="(card, index) in list.cards" class="card card-body">
+
+      <div v-for="(card, index) in list.cards" class="card card-body mb-3">
         {{ card.name }}
       </div>
+
+      <div class="card card-body">
+        <textarea v-model="messages[list.id]" class="form-control"></textarea>
+        <button @click="submitMessages(list.id)" class="btn btn-primary">Add</button>
+      </div>
     </div>
-  </div>
+  </draggable>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+
 export default {
   props: ["original_lists"],
+  components: { draggable },
+  data: function () {
+    return {
+      messages: {},
+      lists: this.original_lists
+    }
+  },
+  methods: {
+    submitMessages (list_id) {
+      var data = new FormData
+      data.append("card[list_id]", list_id)
+      data.append("card[name]", this.messages[list_id])
+
+      Rails.ajax({
+        url: "/cards",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: (data) => {
+          const index = this.lists.findIndex(item => item.id == list_id);
+          this.lists[index].cards.push(data);
+          this.messages[list_id] = undefined;
+        }
+      })
+    },
+    listMoved (event) {
+      var data = new FormData
+      data.append("list[position]", event.newIndex + 1)
+
+      Rails.ajax({
+        url: `lists/${this.lists[event.newIndex].id}/move`,
+        type: "PATCH",
+        data: data,
+        dataType: "json"
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+dragArea {
+  min-height: 20px;
 }
 </style>
